ffd4177b2cbb8fe384b213f16955974cf9e58dc3
obi <anokwuruobi@gmail.com>
2022-11-04 16:58:25 +0000

    finnn

---
+++ typings.d.ts
@@ -0,0 +1,55 @@
+interface SanityBody {
+  _createdAt: Date;
+  _id: string;
+  _rev: string;
+  _updatedAt: Date;
+}
+
+export interface Social extends SanityBody {
+  _type: "social";
+  title: string;
+  url: string;
+}
+
+interface Image {
+  _type: "image";
+  asset: {
+    _ref: string;
+    _type: "reference";
+  };
+}
+
+export interface PageInfo extends SanityBody {
+  _type: "pageInfo";
+  backgroundInfo: string;
+  email: string;
+  name: string;
+  phoneNumber: string;
+  role: string;
+  profilePic: Image;
+  heroImage: Image;
+  address: string;
+  about: string;
+}
+
+export interface Technology extends SanityBody {
+  _type: "skill";
+  image: Image;
+  yearsExperience: string;
+  title: string;
+}
+export interface Skill extends SanityBody {
+  _type: "skill";
+  image: Image;
+  yearsExperience: string;
+  title: string;
+}
+export interface Experience extends SanityBody {
+  _type: "experience";
+  company: string;
+  companyImage: Image;
+  dateStarted: Date;
+  dateEnded: Date;
+  jobTitle: string;
+  technologies: Technology[];
+}
--- utils/fetchConfig.ts
+++ utils/fetchConfig.ts
@@ -1,9 +1,11 @@
 export const getEnvironment = () => {
-  let BASE_URL: string;
-  if (process.env.NODE_ENV === "development") {
-    BASE_URL = "http://localhost:3000";
-  } else {
-    BASE_URL = "https://obiport-tech.vercel.app";
-  }
+  // let BASE_URL: string;
+  // if (process.env.NODE_ENV === "development") {
+  //   BASE_URL = "http://localhost:3000";
+  // } else {
+  //   BASE_URL = "https://obiport-tech.vercel.app";
+  // }
+
+  let BASE_URL = "http://localhost:3000";
   return BASE_URL;
 };
